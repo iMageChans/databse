5c2158b5648d8daa61d8f5f0ef99984e3ebbbf04
Chans <imagechans@gmail.com>
2025-03-14 02:31:06 +0800

    create

--- AgentService/AgentService/settings.py
+++ AgentService/AgentService/settings.py
@@ -168,11 +168,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/agent-service/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 媒体文件配置
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/agent-service/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
