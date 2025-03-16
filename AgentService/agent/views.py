dfe8c58cf93e76e824a30ff3c8381e311a7aacd6
Chans <imagechans@gmail.com>
2025-03-16 17:13:05 +0800

    update code

--- AgentService/agent/views.py
+++ AgentService/agent/views.py
@@ -1,5 +1,6 @@
 import json
 
+from assistant.constants import FREE_RELATIONSHIP_OPTIONS, FREE_NICKNAME_OPTIONS, FREE_PERSONALITY_OPTIONS
 from utils.permissions import IsAuthenticatedExternal
 from .serializers import AgentInputSerializer
 from agent.manager import *
@@ -7,8 +8,9 @@ from utils.mixins import *
 from rest_framework.viewsets import GenericViewSet
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from assistant.models import UsersAssistantTemplates
+from assistant.models import UsersAssistantTemplates, AssistantTemplates
 from engines.models import Engines
+from assistants.models import AssistantsConfigs
 
 
 class AgentViewSet(CreateModelMixin,
@@ -54,27 +56,55 @@ class AgentViewSet(CreateModelMixin,
         user_template_id = validated_data.get("user_template_id", None)
         is_premium = request.remote_user.get('is_premium')
 
-        custom_prompt = UsersAssistantTemplates.objects.filter(user_id=user_id).first().prompt_template
+        # 获取用户模板，如果不存在则创建默认模板
+        user_template = UsersAssistantTemplates.objects.filter(user_id=user_id).first()
+        if not user_template:
+            # 获取默认的助手模板
+            default_template = AssistantTemplates.objects.filter(is_default=True).first()
+            if not default_template:
+                return Response({
+                    "status": "error",
+                    "message": "系统中没有默认模板",
+                    "data": {
+                        "content": {}
+                    }
+                }, status=status.HTTP_404_NOT_FOUND)
+
+            # 创建默认的助手配置
+            default_config = {
+                'user_id': user_id,
+                'name': '默认配置',
+                'relationship': FREE_RELATIONSHIP_OPTIONS[0],
+                'nickname': FREE_NICKNAME_OPTIONS[0],
+                'personality': FREE_PERSONALITY_OPTIONS[0],
+                'greeting': '你好！有什么我可以帮助你的吗？',
+                'dialogue_style': '友好',
+                'is_public': False
+            }
+
+            # 创建新的默认配置
+            config = AssistantsConfigs.objects.create(**default_config)
+
+            # 生成提示词
+            prompt_template = default_template.prompt_template
+            prompt = prompt_template.replace('{relationship}', config.relationship)
+            prompt = prompt.replace('{nickname}', config.nickname)
+            prompt = prompt.replace('{personality}', config.personality)
+            prompt = prompt.replace('{greeting}', config.greeting or '')
+            prompt = prompt.replace('{dialogue_style}', config.dialogue_style or '')
+
+            # 创建用户助手模板
+            user_template = UsersAssistantTemplates.objects.create(
+                user_id=user_id,
+                name='默认模板',
+                prompt_template=prompt,
+                is_default=True,
+                is_premium_template=False
+            )
+
+        custom_prompt = user_template.prompt_template
 
         engine = Engines.objects.get(name=model_name)
-
-        logger.info(f"当前engine.name: {engine.name}")
-        logger.info(f"当前engine.base_url: {engine.base_url}")
-
-        try:
-            if user_template_id and is_premium:
-                user_template = UsersAssistantTemplates.objects.get(user_id=user_id, id=user_template_id,
-                                                                    is_premium_template=True)
-                custom_prompt = user_template.prompt_template
-            elif user_template_id and not is_premium:
-                user_template = UsersAssistantTemplates.objects.get(user_id=user_id, id=user_template_id,
-                                                                    is_premium_template=False)
-                custom_prompt = user_template.prompt_template
-            elif user_template_id and not is_premium:
-                user_template = UsersAssistantTemplates.objects.get(user_id=user_id, is_default=True)
-                custom_prompt = user_template.prompt_template
-        except:
-            pass  # 如果出错，使用默认模板
 
         assistant = AccountingAssistant(
             api_key=engine.api_key,
@@ -87,8 +117,6 @@ class AgentViewSet(CreateModelMixin,
         )
 
         result = assistant.process_input(user_input=users_input, session_id=str(user_id), ai_config=custom_prompt)
-
-        print(result)
 
         response_content = result['response']
 
