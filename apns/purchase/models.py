03c9263a8107951d6ec756cf2fb3a6463e311c17
Chans <imagechans@gmail.com>
2025-03-19 14:39:00 +0800

    create

--- apns/purchase/models.py
+++ apns/purchase/models.py
@@ -10,6 +10,21 @@ class Purchase(models.Model):
         ('failed', 'Failed'),
     ]
 
+    NOTIFY_CHOICES = [
+        ('INITIAL_BUY', '用户首次订阅'),
+        ('DID_CHANGE_RENEWAL_STATUS', '订阅状态更改'),
+        ('INTERACTIVE_RENEWAL', '通过互动方式续订订阅'),
+        ('DID_CHANGE_RENEWAL_PREF', "订阅计划更改"),
+        ('CONSUMPTION_REQUEST', '发起退款'),
+        ('CANCEL', '退订'),
+        ('DID_FAIL_TO_RENEW', '账单问题而未能续订的订阅'),
+        ('DID_RECOVER', '指示之前未能续订的已过期订阅成功自动续订'),
+        ('DID_RENEW', '客户的订阅已成功自动续订至新的交易周期'),
+        ('PRICE_INCREASE_CONSENT', '用户同意自动续订的价格调整，继续续订'),
+        ('REFUND', '已成功退还了应用内购买的消耗品、非消耗品或非续订订阅的交易'),
+        ('REVOKE', '用户通过家庭共享获得的内购项目不再通过共享可用')
+    ]
+
     user_id = models.IntegerField('用户ID', db_index=True, help_text='UserCenter的用户ID')
     app_id = models.CharField("具体苹果的应用", max_length=255, blank=True, null=True)
     product_id = models.CharField(max_length=255, blank=True, null=True)
@@ -22,6 +37,8 @@ class Purchase(models.Model):
     is_successful = models.BooleanField(default=False)
     created_at = models.DateTimeField(_('创建时间'), auto_now_add=True)
     updated_at = models.DateTimeField(_('更新时间'), auto_now=True)
+
+    notification_type = models.CharField('订单通知类型', max_length=255, blank=True, null=True, choices=STATUS_CHOICES)
     
     # 添加状态字段，便于跟踪订单状态
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
