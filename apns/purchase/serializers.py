9f6dd5e2e6d2762a14dc1bc8da7624fad2c77efd
Chans <imagechans@gmail.com>
2025-03-19 18:53:48 +0800

    create

--- apns/purchase/serializers.py
+++ apns/purchase/serializers.py
@@ -21,8 +21,8 @@ class VerifyReceiptSerializer(serializers.Serializer):
 
 class NotificationSerializer(serializers.Serializer):
     """苹果通知序列化器"""
-    notification_type = serializers.CharField(required=True, help_text='通知类型')
-    unified_receipt = serializers.DictField(required=True, help_text='统一收据信息')
+    notification_type = serializers.CharField(required=False, help_text='通知类型')
+    unified_receipt = serializers.DictField(required=False, help_text='统一收据信息')
     auto_renew_status = serializers.BooleanField(required=False, help_text='自动续订状态')
     auto_renew_product_id = serializers.CharField(required=False, allow_null=True, help_text='自动续订产品ID')
     environment = serializers.CharField(required=False, help_text='环境（沙盒/生产）')
